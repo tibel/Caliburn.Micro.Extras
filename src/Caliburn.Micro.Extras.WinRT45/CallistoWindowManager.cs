19f47f93931655c0ce23bfd1408d91130d1b70bb
Thomas Ibel <tibel@users.noreply.github.com>
2013-04-07 20:08:58 +0200

    add CallistoWindowManager

---
+++ src/Caliburn.Micro.Extras.WinRT45/CallistoWindowManager.cs
@@ -0,0 +1,64 @@
+﻿namespace Caliburn.Micro.Extras {
+    using Callisto.Controls;
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Windows.UI.Xaml;
+
+    /// <summary>
+    /// An implementation of the <see cref="IWindowManager" /> using Callisto
+    /// </summary>
+    public class CallistoWindowManager : CallistoSettingsWindowManager, IWindowManager {
+        /// <summary>
+        /// Shows a popup dialog for the specified model relative to the <paramref name="placementTarget"/>.
+        /// </summary>
+        /// <param name="rootModel">The root model.</param>
+        /// <param name="placementTarget">The placement target.</param>
+        /// <param name="viewSettings">The optional dialog settings.</param> 
+        public void ShowFlyout(object rootModel, UIElement placementTarget, IDictionary<string, object> viewSettings = null) {
+            var view = ViewLocator.LocateForModel(rootModel, null, null);
+            ViewModelBinder.Bind(rootModel, view, null);
+
+            var flyout = new Flyout
+                {
+                    Content = view,
+                    PlacementTarget = placementTarget,
+                };
+
+            ApplySettings(flyout, viewSettings);
+            flyout.IsOpen = true;
+
+            var deactivator = rootModel as IDeactivate;
+            if (deactivator != null) {
+                EventHandler<object> closed = null;
+                closed = (s, e) => {
+                    deactivator.Deactivate(true);
+                    flyout.Closed -= closed;
+                };
+
+                flyout.Closed += closed;
+            }
+
+            var activator = rootModel as IActivate;
+            if (activator != null) {
+                activator.Activate();
+            }
+        }
+
+        static bool ApplySettings(object target, IEnumerable<KeyValuePair<string, object>> settings) {
+            if (settings == null)
+                return false;
+
+            var type = target.GetType();
+
+            foreach (var pair in settings) {
+                var propertyInfo = type.GetRuntimeProperty(pair.Key);
+
+                if (propertyInfo != null)
+                    propertyInfo.SetValue(target, pair.Value, null);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/Caliburn.Micro.Extras.WinRT45/IWindowManager.cs
@@ -0,0 +1,17 @@
+﻿namespace Caliburn.Micro.Extras {
+    using System.Collections.Generic;
+    using Windows.UI.Xaml;
+
+    /// <summary>
+    /// A service that manages windows.
+    /// </summary>
+    public interface IWindowManager : ISettingsWindowManager {
+        /// <summary>
+        /// Shows a popup dialog for the specified model relative to the <paramref name="placementTarget"/>.
+        /// </summary>
+        /// <param name="rootModel">The root model.</param>
+        /// <param name="placementTarget">The placement target.</param>
+        /// <param name="viewSettings">The optional dialog settings.</param> 
+        void ShowFlyout(object rootModel, UIElement placementTarget, IDictionary<string, object> viewSettings = null);
+    }
+}
