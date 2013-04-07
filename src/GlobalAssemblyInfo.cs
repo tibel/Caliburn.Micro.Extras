3e2c73dd3107d08ac057a2ac46b74f86fe88dc55
Thomas Ibel <tibel@users.noreply.github.com>
2013-04-07 17:13:48 +0200

    create project structure

---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Caliburn.Micro.Extras")]
+[assembly: AssemblyDescription("Some additions to Caliburn.Micro")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyProduct("Caliburn.Micro.Extras")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright © 2013")]
+
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: Guid("b9595e44-9830-4555-ba01-b36d216a1e16")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
