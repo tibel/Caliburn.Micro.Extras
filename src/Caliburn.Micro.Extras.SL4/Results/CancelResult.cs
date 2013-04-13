aa46c2a659bf2ffa8329473477cd216f907b64f7
Thomas Ibel <tibel@users.noreply.github.com>
2013-04-13 08:25:49 +0200

    Add CancelResult

---
+++ src/Caliburn.Micro.Extras.SL4/Results/CancelResult.cs
@@ -0,0 +1,21 @@
+﻿namespace Caliburn.Micro.Extras {
+    using System;
+
+    /// <summary>
+    /// A result that is always canceled.
+    /// </summary>
+    public class CancelResult : IResult {
+        /// <summary>
+        /// Executes the result using the specified context.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public void Execute(ActionExecutionContext context) {
+            Completed(this, new ResultCompletionEventArgs {WasCancelled = true});
+        }
+
+        /// <summary>
+        /// Occurs when execution has completed.
+        /// </summary>
+        public event EventHandler<ResultCompletionEventArgs> Completed = delegate { };
+    }
+}
