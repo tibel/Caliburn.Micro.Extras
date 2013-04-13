85d704caa61a97d1f7c7718b97f9bcfc45eb5e45
Thomas Ibel <tibel@users.noreply.github.com>
2013-04-13 18:47:57 +0200

    Add ExternalModuls support to WP7 and WP8

---
+++ src/Caliburn.Micro.Extras.WP71/ExternalModules/ModuleBootstrapper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Caliburn.Micro.Extras.ExternalModules {
+    using System.Linq;
+
+    /// <summary>
+    /// Interface for module bootstrappers.
+    /// </summary>
+    public interface IModuleBootstrapper {
+        /// <summary>
+        /// Initializes the module.
+        /// </summary>
+        void Initialize();
+    }
+
+    /// <summary>
+    /// Base class for all module bootstrappers.
+    /// </summary>
+    public abstract class ModuleBootstrapperBase : IModuleBootstrapper {
+        /// <summary>
+        /// Gets or sets the IoC container.
+        /// </summary>
+        public IPhoneContainer Container { get; set; }
+
+        /// <summary>
+        /// Initializes the module.
+        /// </summary>
+        public virtual void Initialize() {
+            var phoneContainer = Container as PhoneContainer;
+            if (phoneContainer == null)
+                throw new InvalidOperationException("Container has to be initialized.");
+
+            ConfigureStorageMechanismsAndWorkers(phoneContainer);
+            Configure(phoneContainer);
+        }
+
+        /// <summary>
+        /// Identify, load and configure all instances of <see cref="IStorageMechanism"/> and <see cref="IStorageHandler"/> 
+        /// that are defined in the assembly associated with this bootstrapper.
+        /// </summary>
+        /// <param name="phoneContainer">The currently configured <see cref="PhoneContainer"/>.</param>
+        /// <remarks>
+        /// Caliburn Micro will automatically load storage handlers and storage mechanisms from the assemblies configured
+        /// in <see cref="AssemblySource.Instance"/> when <see cref="PhoneContainer.RegisterPhoneServices"/> is first invoked.
+        /// Since the purpose of this bootstrapper is to allow the delayed loading of assemblies, it makes sense to locate
+        /// the storage handlers alongside the view models in the same assembly. 
+        /// </remarks>
+        private void ConfigureStorageMechanismsAndWorkers(PhoneContainer phoneContainer) {
+            var coordinator = (StorageCoordinator) (phoneContainer.GetInstance(typeof (StorageCoordinator), null));
+            var assembly = GetType().Assembly;
+
+            phoneContainer.AllTypesOf<IStorageMechanism>(assembly);
+            phoneContainer.AllTypesOf<IStorageHandler>(assembly);
+
+            phoneContainer.GetAllInstances(typeof (IStorageMechanism)).
+                           Where(m => ReferenceEquals(m.GetType().Assembly, assembly)).
+                           Apply(m => coordinator.AddStorageMechanism((IStorageMechanism) m));
+
+            phoneContainer.GetAllInstances(typeof (IStorageHandler)).
+                           Where(h => ReferenceEquals(h.GetType().Assembly, assembly)).
+                           Apply(h => coordinator.AddStorageHandler((IStorageHandler) h));
+        }
+
+        /// <summary>
+        /// Override to setup the IoC container for this module.
+        /// </summary>
+        /// <param name="container">The parent IoC container.</param>
+        protected abstract void Configure(PhoneContainer container);
+    }
+}
