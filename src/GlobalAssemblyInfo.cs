9c3612addd8a0f8f39645593124f5ab43c778a72
Thomas Ibel <tibel@users.noreply.github.com>
2013-11-05 08:45:57 +0100

    update to VS2013 (remove SL4 and WP71 support)

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
@@ -9,12 +10,16 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 [assembly: AssemblyProduct("Caliburn.Micro.Extras")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright © 2013")]
+[assembly: AssemblyCompany("https://github.com/tibel/Caliburn.Micro.Extras")]
+[assembly: AssemblyCopyright("Copyright © Thomas Ibel 2013")]
+
+#if !WinRT
+[assembly: CLSCompliant(true)]
+#endif
 
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: Guid("b9595e44-9830-4555-ba01-b36d216a1e16")]
 
-[assembly: AssemblyVersion("1.0.6")]
-[assembly: AssemblyFileVersion("1.0.6")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
